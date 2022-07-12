fd01a9fe586fff938e958f4feddaaaf6d415e303
Maria del Pilar Payo <pilipayo@gmail.com>
2022-07-12 18:23:28 -0300

    -

---
+++ final/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="jumbotron">
+    <h1> Final PAYO, MARIA DEL PILAR </h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
